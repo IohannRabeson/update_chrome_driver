deb1c82475c7c8376c0fbc0d92fb04ab21043b7e
Iohann Rabeson <IohannRabeson@users.noreply.github.com>
2023-01-20 22:41:52 -0500

    Windows (#2)

--- src/parsers.rs
+++ src/parsers.rs
@@ -1,6 +1,6 @@
 use crate::Version;
 use nom::bytes::complete::tag;
-use nom::character::complete::{char, digit1};
+use nom::character::complete::{char, digit1, space0};
 use nom::combinator::map_res;
 use nom::sequence::tuple;
 use nom::IResult;
@@ -37,23 +37,43 @@ pub fn parse_version_numbers(input: &str) -> IResult<&str, Version> {
     ))
 }
 
-/// Parse the version in the output of the command `chromedriver --version`.
-/// Example: `ChromeDriver 89.0.4389.23 (61b08ee2c50024bab004e48d2b1b083cdbdac579-refs/branch-heads/4389@{#294})`
-pub fn parse_chromedriver_version_output(input: &str) -> IResult<&str, Version> {
-    let (input, _) = tag("ChromeDriver ")(input)?;
+pub fn parse_version_output<'a>(input: &'a str, application: &'a str) -> IResult<&'a str, Version> {
+    let (input, _) = tag(application)(input)?;
+    let (input, _) = space0(input)?;
 
     parse_version_numbers(input)
 }
 
+/// Parse the version in the output of the command `chromedriver --version`.
+/// Example: `ChromeDriver 89.0.4389.23 (61b08ee2c50024bab004e48d2b1b083cdbdac579-refs/branch-heads/4389@{#294})`
+pub fn parse_chromedriver_version_output(input: &str) -> IResult<&str, Version> {
+    parse_version_output(input, "ChromeDriver")
+}
+
+#[cfg(not(target_os = "windows"))]
 pub fn parse_chromium_version_output(input: &str) -> IResult<&str, Version> {
-    let (input, _) = tag("Google Chrome ")(input)?;
+    parse_version_output(input, "Google Chrome")
+}
+
+#[cfg(target_os = "windows")]
+pub fn parse_version_from_register(input: &str) -> IResult<&str, Version> {
+    use nom::character::complete::space1;
+
+    let (input, _) = tag("\r\nHKEY_CURRENT_USER\\Software\\Google\\Chrome\\BLBeacon\r\n")(input)?;
+    let (input, _) = space1(input)?;
+    let (input, _) = tag("version")(input)?;
+    let (input, _) = space1(input)?;
+    let (input, _) = tag("REG_SZ")(input)?;
+    let (input, _) = space1(input)?;
 
     parse_version_numbers(input)
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::parsers::{parse_chromedriver_version_output, parse_chromium_version_output};
+    use crate::parsers::parse_chromedriver_version_output;
+    #[cfg(not(target_os = "windows"))]
+    use crate::parsers::parse_chromium_version_output;
     use crate::Version;
     use nom::Finish;
     use test_case::test_case;
@@ -68,6 +88,7 @@ mod tests {
         assert_eq!(expected, result);
     }
 
+    #[cfg(not(target_os = "windows"))]
     #[test_case("Google Chrome 109.0.5414.87", Some(Version::new(109, 0, 5414, 87)) ; "basic")]
     fn test_parse_browser_version_output(input: &str, expected: Option<Version>) {
         let result = parse_chromium_version_output(input)
